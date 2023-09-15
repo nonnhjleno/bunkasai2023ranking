874be31227c91012d57f626a6b3e5dbb477e79dc
nonnhjleno <taaaisei999@gmail.com>
2023-09-15 17:39:41 +0900

    1秒ごとに更新するfetchを作った

--- src/App.js
+++ src/App.js
@@ -1,25 +1,31 @@
-import logo from './logo.svg';
-import './App.css';
+import useSWR from 'swr';
+import axios from 'axios';
+import { useState, useEffect } from 'react';
+
+const fetcher = (url) => axios.get(url).then((res) => res.data);
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  const [data, setData] = useState(null);
+
+  const { data: swrData } = useSWR('http://127.0.0.1:8000/api/test', fetcher, {
+    refreshInterval: 1000,
+    onError: (err) => {
+      console.log(err);
+    },
+    onSuccess: (data) => {
+      setData(data);
+    },
+  });
+
+  useEffect(() => {
+    if (swrData) {
+      setData(swrData);
+    }
+  }, [swrData]);
+
+  if(!data) return ( <p>ロード中...</p>);
+  else return (<p>{JSON.stringify(data)}</p>);
+
 }
 
 export default App;
